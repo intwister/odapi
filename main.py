1e52e30c35c0f3c9686f07614e190c9e03309303
justinfoley <github@foley2000.com>
2017-02-26 19:30:47 +0000

    First set of files - the basic end to end function

---
+++ main.py
@@ -0,0 +1,24 @@
+import odapi_client
+import TM1638
+
+DIO = 17
+CLK = 21
+STB = 22
+config = odapi_client.OdApiConfg.load("config.json")
+
+client = odapi_client.OdApiClient(config)
+# words = client.get_word_list({"registers": "Coarse_Slang"})
+words = client.get_word_list({"lexicalCategory": "noun,adjective"})
+print words
+
+display = TM1638.TM1638(DIO, CLK, STB, sleep_time=0.0005)
+display.enable(intensity=3)
+
+for word in words:
+    try:
+        definition = client.define_word(word)
+        print definition
+        display.scroll_text("")
+        display.scroll_text("- {} - {}".format(word, definition))
+    except odapi_client.ODAPIClientException as e:
+        pass
